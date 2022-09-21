d186d864d5d8e2231f818dc13aaa2d51c11349ef
Naoya Matsuo@SYS <naoya.matsuo2@grp.tmnf.jp>
2022-09-21 14:43:58 +0900

    Day4

--- work/dl_katakana/2_notebook/common/layers.py
+++ work/dl_katakana/2_notebook/common/layers.py
@@ -1,84 +1,116 @@
+# coding: utf-8
 import numpy as np
+from common.functions import *
 from collections import OrderedDict
 
-def cross_entropy_error(y, t):
+def numerical_gradient(f, x, W, t):
     """
-    y : 出力値(通常は、0-1の確率)  
-    t : 正解値(通常は、0or1)  
+    f : 目的関数
+    x : 入力データ
+    t : 正解データ   
     """
-    if y.ndim==1:
-        t = t.reshape(1, -1)
-        y = y.reshape(1, -1)
+    h = 1e-4 # 0.0001
+    grad = np.zeros_like(W)
+    
+    it = np.nditer(W, flags=['multi_index'])
+    
+    while not it.finished:
+        idx = it.multi_index # indexを取り出す
+        tmp_val = W[idx]
         
-    batch_size = y.shape[0]
-    delta = 1e-7
-    return -np.sum( t * np.log(y + delta)) / batch_size
-
-def softmax(x):
-    if x.ndim == 2:
-        x = x.T
-        x = x - np.max(x, axis=0)
-        y = np.exp(x) / np.sum(np.exp(x), axis=0)
-        return y.T 
-
-    x = x - np.max(x) # オーバーフロー対策
-    return np.exp(x) / np.sum(np.exp(x))
+        W[idx] = tmp_val + h
+        fxh1 = f(x, t)
+        
+        W[idx] = tmp_val - h 
+        fxh2 = f(x, t)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        
+        W[idx] = tmp_val # 値を元に戻す
+        
+        it.iternext()    # 次のindexへ進める
+        
+    return grad
 
 class ReLU:
     def __init__(self):
         self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+
+        return out
+
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+        return dx
+
+    
+class LeakyReLU:
+    def __init__(self, alpha=0.1):
+        self.mask = None
+        self.alpha = alpha
+        self.mask = None
         
     def forward(self, x):
         self.mask = (x <= 0)
-        out = x.copy() #参照渡しではなく複製する
-        out[self.mask] = 0
+        out = x.copy()
+        out[self.mask] *= self.alpha
         return out
-    
+
     def backward(self, dout):
-        """
-        dout : float, 上流(出力)側の勾配
-        """        
-        dout[self.mask] = 0
-        dLdx = dout
-        return dLdx
+        dout[self.mask] *= self.alpha
+        dx = dout
+        return dx
     
+    
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        out = sigmoid(x)
+        self.out = out
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+
+        return dx
+
+
 class Affine:
     def __init__(self, W, b):
         self.W =W
         self.b = b
         
-        # 初期値
         self.x = None
         self.original_x_shape = None
-        self.dW = None # 重みの微分
-        self.db = None # バイアスの微分
+        # 重み・バイアスパラメータの微分
+        self.dW = None
+        self.db = None
 
     def forward(self, x):
-        """
-        順伝播
-        """
-        # 値の保持
+        # テンソル対応(画像形式のxに対応させる)
+        self.original_x_shape = x.shape
+        x = x.reshape(x.shape[0], -1)
         self.x = x
 
-        # 順伝播
         out = np.dot(self.x, self.W) + self.b
 
         return out
 
     def backward(self, dout):
-        """
-        逆伝播
-        dout : float, 上流(出力)側の勾配
-        """
-        # dxは前の層に伝える必要がある
         dx = np.dot(dout, self.W.T)
-        
-        # dWとdbは、勾配法の計算に使われるので、値を保持しておく
         self.dW = np.dot(self.x.T, dout)
-        self.db = np.sum(dout, axis=0) # N個の合計になる
+        self.db = np.sum(dout, axis=0)
         
+        dx = dx.reshape(*self.original_x_shape)  # 入力データの形状に戻す（テンソル対応）
         return dx
-    
+
+
 class SoftmaxWithLoss:
     def __init__(self):
         
@@ -101,54 +133,214 @@ class SoftmaxWithLoss:
         """
         逆伝播
         伝播する値をバッチサイズで割ること
+        dout=1は、他のレイヤと同じ使い方ができるように設定しているダミー変数
         """
         batch_size = self.t.shape[0]
         dx = (self.y - self.t) / batch_size
 
         return dx
 
-    
-    
-def numerical_gradient(f, W):
-    """
-    全ての次元について、個々の次元だけの微分を求める
-    f : 関数
-    W : 偏微分を求めたい場所の座標。多次元配列
-    """
-    h = 1e-4 # 0.0001
-    grad = np.zeros_like(W)
-    
-    it = np.nditer(W, flags=['multi_index'])
-    
-    while not it.finished:
-        idx = it.multi_index
-        tmp_val = W[idx]
-        
-        W[idx] = tmp_val + h
-        fxh1 = f(W)
-        
-        W[idx] = tmp_val - h 
-        fxh2 = f(W)
-        grad[idx] = (fxh1 - fxh2) / (2*h)
-        
-        W[idx] = tmp_val # 値を元に戻す
-        
-        # 次のindexへ進める
-        it.iternext()   
-        
-    return grad
+class MeanSquaredLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None # 2乗和誤差損失関数の出力
+        self.t = None # 教師データ
 
+    def forward(self, x, t):
+        self.t = t
+        self.y = x #恒等関数
+        self.loss = mean_squared_error(self.y, self.t)
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+        return dx
+    
+    
+class Dropout:
+    def __init__(self, dropout_ratio=0.5):
+        self.dropout_ratio = dropout_ratio
+        self.mask = None
+
+    def forward(self, x, train_flg=True):
+        if train_flg:
+            self.mask = np.random.rand(*x.shape) > self.dropout_ratio
+            return x * self.mask
+        else:
+            return x * (1.0 - self.dropout_ratio)
+
+    def backward(self, dout):
+        return dout * self.mask
+
+    
+class BatchNormalization:
+    def __init__(self, gamma, beta, rho=0.9, moving_mean=None, moving_var=None):
+        self.gamma = gamma # スケールさせるためのパラメータ, 学習によって更新させる.
+        self.beta = beta # シフトさせるためのパラメータ, 学習によって更新させる
+        self.rho = rho # 移動平均を算出する際に使用する係数
+
+        # 予測時に使用する平均と分散
+        self.moving_mean = moving_mean # muの移動平均
+        self.moving_var = moving_var        # varの移動平均
+        
+        # 計算中に算出される値を保持しておく変数群
+        self.batch_size = None
+        self.x_mu = None
+        self.x_std = None        
+        self.std = None
+        self.dgamma = None
+        self.dbeta = None
+
+    def forward(self, x, train_flg=True):
+        """
+        順伝播計算
+        x :  CNNの場合は4次元、全結合層の場合は2次元  
+        """
+        if x.ndim == 4:
+            """
+            画像形式の場合
+            """
+            N, C, H, W = x.shape
+            x = x.transpose(0, 2, 3, 1) # NHWCに入れ替え
+            x = x.reshape(N*H*W, C) # (N*H*W,C)の2次元配列に変換
+            out = self.__forward(x, train_flg)
+            out = out.reshape(N, H, W, C)# 4次元配列に変換
+            out = out.transpose(0, 3, 1, 2) # 軸をNCHWに入れ替え
+        elif x.ndim == 2:
+            """
+            画像形式以外の場合
+            """
+            out = self.__forward(x, train_flg)           
+            
+        return out
+            
+    def __forward(self, x, train_flg, epsilon=1e-8):
+        """
+        x : 入力. n×dの行列. nはあるミニバッチのバッチサイズ. dは手前の層のノード数
+        """
+        if (self.moving_mean is None) or (self.moving_var is None):
+            N, D = x.shape
+            self.moving_mean = np.zeros(D)
+            self.moving_var = np.zeros(D)
+                        
+        if train_flg:
+            """
+            学習時
+            """
+            # 入力xについて、nの方向に平均値を算出. 
+            mu = np.mean(x, axis=0) # 要素数d個のベクトル
+            
+            # 入力xから平均値を引く
+            x_mu = x - mu   # n*d行列
+            
+            # 入力xの分散を求める
+            var = np.mean(x_mu**2, axis=0)  # 要素数d個のベクトル
+            
+            # 入力xの標準偏差を求める(epsilonを足してから標準偏差を求める)
+            std = np.sqrt(var + epsilon)  # 要素数d個のベクトル
+            
+            # 標準化
+            x_std = x_mu / std  # n*d行列
+            
+            # 値を保持しておく
+            self.batch_size = x.shape[0]
+            self.x_mu = x_mu
+            self.x_std = x_std
+            self.std = std
+            self.moving_mean = self.rho * self.moving_mean + (1-self.rho) * mu
+            self.moving_var = self.rho * self.moving_var + (1-self.rho) * var            
+        else:
+            """
+            予測時
+            """
+            x_mu = x - self.moving_mean # n*d行列
+            x_std = x_mu / np.sqrt(self.moving_var + epsilon) # n*d行列
+            
+        # gammaでスケールし、betaでシフトさせる
+        out = self.gamma * x_std + self.beta # n*d行列
+        return out
+
+    def backward(self, dout):
+        """
+        逆伝播計算
+        dout : CNNの場合は4次元、全結合層の場合は2次元  
+        """
+        if dout.ndim == 4:
+            """
+            画像形式の場合
+            """            
+            N, C, H, W = dout.shape
+            dout = dout.transpose(0, 2, 3, 1) # NHWCに入れ替え
+            dout = dout.reshape(N*H*W, C) # (N*H*W,C)の2次元配列に変換
+            dx = self.__backward(dout)
+            dx = dx.reshape(N, H, W, C)# 4次元配列に変換
+            dx = dx.transpose(0, 3, 1, 2) # 軸をNCHWに入れ替え
+        elif dout.ndim == 2:
+            """
+            画像形式以外の場合
+            """
+            dx = self.__backward(dout)
+
+        return dx
+
+    def __backward(self, dout):
+        """
+        ここを完成させるには、計算グラフを理解する必要があり、実装にかなり時間がかかる.
+        """
+        
+        # betaの勾配
+        dbeta = np.sum(dout, axis=0)
+        
+        # gammaの勾配(n方向に合計)
+        dgamma = np.sum(self.x_std * dout, axis=0)
+        
+        # Xstdの勾配
+        a1 = self.gamma * dout
+        
+        # Xmuの勾配(1つ目)
+        a2 = a1 / self.std
+        
+        # 標準偏差の逆数の勾配(n方向に合計)
+        a3 = np.sum(a1 * self.x_mu, axis=0)
+
+        # 標準偏差の勾配
+        a4 = -(a3) / (self.std * self.std)
+        
+        # 分散の勾配
+        a5 = 0.5 * a4 / self.std
+        
+        # Xmuの2乗の勾配
+        a6 = a5 / self.batch_size
+        
+        # Xmuの勾配(2つ目)
+        a7 = 2.0  * self.x_mu * a6
+        
+        # muの勾配
+        a8 = np.sum(-(a2+a7), axis=0)
+
+        # Xの勾配
+        dx = a2 + a7 +  a8 / self.batch_size # 第3項はn方向に平均
+        
+        self.dgamma = dgamma
+        self.dbeta = dbeta
+        
+        return dx
+
+    
 
 class TwoLayerNet:
 
     def __init__(self, input_size, hidden_size, output_size, weight_init_std = 0.01):
         # 重みの初期化
         self.params = {}
+        np.random.seed(1234)
         self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
         self.params['b1'] = np.zeros(hidden_size)
         self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size) 
         self.params['b2'] = np.zeros(output_size)
 
+        
         # レイヤの生成
         self.layers = OrderedDict() # 順番付きdict形式. ただし、Python3.6以降は、普通のdictでもよい
         self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
@@ -162,7 +354,7 @@ class TwoLayerNet:
         x : 入力
         """
         for layer in self.layers.values():
-            # 入力されたxを上書きしていく = 順伝播計算
+            # 入力されたxを更新していく = 順伝播計算
             x = layer.forward(x)
         
         return x
@@ -220,18 +412,16 @@ class TwoLayerNet:
         return grads
     
 
-    def numerical_gradient(self, x, t):
+    def numerical_gradient_(self, x, t):
         """
         勾配確認用
-        x:入力データ, t:教師データ        
+        x:入力データ, t:正解データ        
         """
-        
-        loss_W = lambda W: self.loss(x, t)
-
         grads = {}
-        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
-        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
-        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
-        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+        f = self.loss
+        grads['W1'] = numerical_gradient(f, x, self.params['W1'], t)
+        grads['b1'] = numerical_gradient(f, x, self.params['b1'], t)
+        grads['W2'] = numerical_gradient(f, x, self.params['W2'], t)
+        grads['b2'] = numerical_gradient(f, x, self.params['b2'], t)
 
-        return grads
+        return grads    
